709dd95ef3d0cfeb636b10d0b3e18cb2018bce75
yuxizhe <yuxizhe2008@163.com>
2016-05-19 10:05:11 +0800

    pop push shift unshifgit show

---
+++ 2-6.js
@@ -0,0 +1,43 @@
+function $(id) {
+	return document.getElementById(id);
+}
+
+
+var numbers= new Array;
+
+function render(){
+	var temp = '';
+	for (var i = 0; i < numbers.length; i++) {
+		temp+= "<div class='number'>"+ numbers[i] +"</div>";
+	}
+	$('show').innerHTML = temp;
+}
+
+function getInput(){
+	return $('input').value;
+}
+
+function init(){
+	$('rightin').onclick = function(){
+		numbers.push(getInput());
+		render();
+	}
+	$('rightout').onclick = function(){
+		numbers.pop();
+		render();
+	}
+	$('leftin').onclick = function(){
+		numbers.unshift(getInput());
+		render();
+	}
+	$('leftout').onclick = function(){
+		numbers.shift();
+		render();
+	}
+
+
+}
+
+window.onload = function () {
+	init();
+}
