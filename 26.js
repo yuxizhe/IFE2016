eca607ce8af69aee23b83106184ecfc081ccb57e
yuxizhe <yuxizhe2008@163.com>
2016-06-01 12:01:11 +0800

    对象的定义 很有意思

--- 26.js
+++ 26.js
@@ -1,33 +1,46 @@
 
 
 
-function plane(orbit){
-	this.orbit = orbit;
-	this.state = 0;
+function plane(orbi){
+	var obj={
+		orbit : orbi,
+		energy : 500,
+		state : 0,
+		command : {
+			start: function () {
+				if(obj.energy>0){
+					obj.state = 1;
+				}
+			},
+			stop:function () {
+				obj.state = 0;
+			}
+		},
+		status:{
+			charge:function (num) {
+				obj.energy += num;
+				if(obj.energy>500){
+					obj.energy = 500;
+				}
+			},
+			consume:function(num){
+				if(obj.state == 1){
+					obj.energy -= num;
+				}
+				if (obj.energy <= 0) {
+					obj.state = 0;
+					obj.energy = 0;
+				}
+			},
+		},
+		radio:{
+			
+		}
+	};
+	return obj;
 }
+//对象的定义参考 贝加莱结构体的方式  command status ...
 
-plane.prototype.fly = function(first_argument) {
-	// body...
-};
-
-plane.prototype.stop = function(first_argument) {
-	// body...
-};
-
-plane.prototype.charge = function(first_argument) {
-	// body...
-};
-
-plane.prototype.destory = function(first_argument) {
-	// body...
-};
-
-plane.prototype.method_name = function(first_argument) {
-	// body...
-};
-plane.prototype.MediatorRecive = function(message) {
-	// body...
-};
 
 
 
