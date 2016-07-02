54e4ae80cb5a9d3ec74e9d6399e59b30927201cd
yuxizhe <yuxizhe2008@163.com>
2016-07-02 22:16:40 +0800

    d

---
+++ road/app.js
@@ -0,0 +1,12 @@
+var Application = function (background,rabbit) {
+
+	this.background = background;
+	this.rabbit= rabbit;
+	this.init();
+
+	//addEventListener  click 
+}
+
+Application.prototype.init = function() {
+	
+};
---
+++ road/background.js
@@ -0,0 +1,27 @@
+var Background = function (row,col,target,wallPic) {
+	this.element = document.getquerySelector(target);
+	this.row = row;
+	this.col = col;
+	this.wallPic = wallPic;
+	this.Map = [];
+	for(var i = 0,col = [];i<this.col;i++){
+		this.Map[i]= col;
+	};
+	this.init();
+}
+
+Background.prototype.init = function() {
+	
+};
+
+Background.prototype.randomPath = function() {
+	var i = 0,j=0;
+};
+
+Background.prototype.randomWall = function() {
+	
+};
+
+Background.prototype.render = function() {
+	
+};
