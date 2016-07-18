62d50f8337234dbb9d7bdaab7cbdc0da1a41a3e1
yuxizhe <yuxizhe2008@163.com>
2016-07-18 21:42:39 +0800

    webpack webpack.js webpackAll.js   会自动添加所有require的文件

---
+++ webpack-test/webpack.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+  document.getElementsByTagName('p')[0].innerHTML = "webpack";
+  require("./webpack2.js")
+}
---
+++ webpack-test/webpack2.js
@@ -0,0 +1,3 @@
+var b = document.createElement('p');
+b.innerHTML = "第二个文件成功加载";
+document.getElementsByTagName('p')[0].appendChild(b);
