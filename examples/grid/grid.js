6764b15c138fe3f00adbf468e7ec196267307f02
yuxizhe <yuxizhe2008@163.com>
2016-08-15 17:34:45 +0800

    列表点击排序

--- examples/grid/grid.js
+++ examples/grid/grid.js
@@ -33,7 +33,7 @@ var demo = new Vue({
     gridColumns: ['name', 'power'],
     gridData: [
       { name: 'Chuck Norris', power: Infinity },
-      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Bruce Lee', power: 900 },
       { name: 'Jackie Chan', power: 7000 },
       { name: 'Jet Li', power: 8000 }
     ]
--- vuetest/src/main.js
+++ vuetest/src/main.js
@@ -93,7 +93,24 @@ var aa = new Vue({
 	}
 })
 
-new Vue({
-	
+var grid = new Vue({
+	el:"#grid",
+	data:{
+		searchQuery:"",
+		sortQuery:"",
+		gridColumns:[ "name","score"],
+		gridData:[
+			{ name: "lee", score: 100},
+			{ name: "Jack", score: 32},
+			{ name: "Tom", score: 12},
+			{ name: "AC", score: 89},
+			{ name: "Mike", score: 45},
+			{ name: "Wang", score: 90},
+			{ name: "Will", score: 78},
+			{ name: "Edw", score: 89},
+			{ name: "Mii", score: 90},
+		]
+	}
 })
+
 
