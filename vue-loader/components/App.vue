1d85f031bd84975cf963d42c6d9e896403d3347a
yuxizhe <yuxizhe2008@163.com>
2016-08-22 23:52:06 +0800

    webpack+vueloader  not work now

---
+++ vue-loader/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<li v-for="a in data">a</li>
+	</div>
+</template>
+
+<script>
+	var number=[
+		3,2,34,2,41,5,123,1234,123,51
+	];
+	export default{
+		data:{
+			data:number
+		}
+	}
+</script>
---
+++ vue-loader/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+// 下面这个注意加点  
+import App from './components/App'   
+
+new Vue({
+	el:'body',
+	components:{
+		app:App
+	}
+})
