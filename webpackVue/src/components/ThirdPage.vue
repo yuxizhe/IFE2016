7f0c4e139f0cb47f1afc7a5a1204dd63badd31b5
yuxizhe <yuxizhe2008@163.com>
2016-09-07 14:04:01 +0800

    增加 试用firebase  在使用代理时 firebase login 有问题 ，在路由器中设置全局代理可以解决 curl -i http://ip.cn 可以看到终端是否有代理。 再加上参数 --no-localhost.  firebase会上传设置中public中的所有文件。所以把webpack后的复制到这个文件夹里，在firebase deploy  。注意所有的firebase 操作在mac上我都要加上sudo 才行

--- webpackVue/src/components/ThirdPage.vue
+++ webpackVue/src/components/ThirdPage.vue
@@ -1,3 +1,26 @@
 <template>
-	<div>Thirdpage</div>
+	<div>Firebase</div>
+	<div>{{users.name}}</div>
 </template>
+<script >
+	import firebase from "firebase"
+
+	//firebase = new Firebase();
+
+	var config = {
+    apiKey: "AIzaSyD4az7go2CWyb-Yy_2wHISnfoytLEzUg-4",
+    authDomain: "yuxizhe2008.firebaseapp.com",
+    databaseURL: "https://yuxizhe2008.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+
+  var usersRef = firebase.database().ref('/test');
+
+  export default{
+
+  	firebase:{
+  		users:usersRef
+  	}
+  }
+</script>
