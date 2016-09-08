8d07c044936b2c02e615bd846f2b47e9bcf2453a
yuxizhe <yuxizhe2008@163.com>
2016-09-08 23:07:23 +0800

    // 从hacker news学来的 终于知道怎么修改data数据了 	created(){ 		var a=[]; 		var _this=this; 		usersRef.on('value', snapshot => { 		  a=snapshot.val(); 		 _this.users=a 		}); 	},

--- webpackVue/src/components/ThirdPage.vue
+++ webpackVue/src/components/ThirdPage.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="page3-div">Firebase</div>
+	<div v-for="user in users">{{user.name}}</div>
 	<form  v-on:submit.prevent="addUser">
         <input v-model="newUser.name">
         <input v-model="newUser.email">
@@ -9,9 +10,8 @@
 </template>
 <script >
 	import firebase from "firebase"
-
+	
 	//firebase = new Firebase();
-
 	var config = {
     apiKey: "AIzaSyD4az7go2CWyb-Yy_2wHISnfoytLEzUg-4",
     authDomain: "yuxizhe2008.firebaseapp.com",
@@ -20,20 +20,41 @@
   };
   firebase.initializeApp(config);
 
+
+  var a;
   var usersRef = firebase.database().ref('/test');
 
+  
   export default{
 
-  	data: {
-    newUser: {
-      name: '',
-      email: ''
-    }
-  },
+  	data(){
+  		return{
+		    newUser: {
+		      name: '',
+		      email: ''
+		   			 }
+  				,
+  			users: {name:1}
+		}},
 
-  	// firebase:{
-  	// 	users:usersRef
-  	// },
+	firebase:{
+		users:usersRef
+	},
+	created(){
+		var a=[];
+		var _this=this;
+		usersRef.on('value', snapshot => {
+		  a=snapshot.val();
+
+		  console.log(a);
+		 _this.users=a
+		});
+
+
+
+		// a.on('change',this.users=a)
+	},
+  	
   	 methods: {
     addUser: function () {
       // if (this.isValid) {
